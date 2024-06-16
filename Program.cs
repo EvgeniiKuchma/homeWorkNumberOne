094b8528f759411e3c9083a01aeac1f9c63f0334
evgenii <evgenii@DESKTOP-VCPQ15O>
2024-06-16 14:25:46 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWorkNumberOne
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int crystalPayment = -1;
+            uint blacksmithsСrystals  = 10;
+            bool enoughCrystalsToBuy = true;
+            bool notEnoughCrystalsToBuy = false;
+            float defaultPlantGrowthTime =15.1f;
+            double plantGrowthTimeWithSkillBonus = 0.15;
+            string nameOfUnit = "Riot rat";
+            char letterSpellKey = 'A';
+            ushort timeOfDayHourses = 24;
+            short minutesUntilTheEndOfTheBuff = -15;
+            byte timeOfDaySeconds = 60;
+            long nuberDamageToGodBuff = 1000000000000000000;
+
+
+        }
+    }
+}
